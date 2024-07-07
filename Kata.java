308bcc35f1a0246f16abc9bfb9e264ea38be7fca
Abimbola Jolayemi <abimbolajolayemi111@yahoo.com>
2024-07-07 05:13:31 +0100

    Kata Assignment

--- Kata.java
+++ Kata.java
@@ -3,11 +3,7 @@ public class Kata{
 
 	Scanner input = new Scanner (System.in);
    	
-	public boolean isEven(){
-
-    	System.out.print("Enter a number: ");
-	int number = input.nextInt();
-
+	public boolean isEven(int number){
 	if (number % 2 == 0) 
 		return true;
 	else
@@ -15,14 +11,11 @@ public class Kata{
 
    }
 
-	public boolean isPrimeNumber(){
+	public boolean isPrimeNumber(int number){
 
 	int count = 0;
-	
-	System.out.print("Enter a number: ");
-	int number = input.nextInt();
 
-	for (int num = 1; num <= number; num++){
+	for (int num = 2; num <= number; num++){
 		if (number % num == 0){
 			count++;
 		}
@@ -47,4 +40,56 @@ public class Kata{
 		else
 		return firstNumber / secondNumber;
 	}
+
+	public boolean isPerfectSquare(int number){
+		
+		for (int i = 1; i <= number; i++){
+			if (i * i == number){
+			   return true;
+			}
+		}
+		   	   return false;	
+	}
+
+	public void factorsOfNumbers(int number){
+
+		for (int factor = 1; factor <= number; factor++){
+			if(number % factor == 0)
+			   System.out.println(factor);
+		}
+	}
+
+	public boolean isPalindrome(int number){
+
+		int digit1 = number % 10;
+		number = number / 10;
+
+		int digit2 = number % 10;
+		number = number / 10;
+
+		int digit3 = number % 10;
+		number = number / 10;
+
+		int digit4 = number % 10;
+		number = number / 10;
+
+		int digit5 = number;
+
+		if (digit1 == digit5 && digit2 == digit4)
+			return true;
+		else
+			return false;
+	}
+
+	public long factorial(int number){
+		
+		long factorial = 1;
+
+		for (int index = 1; index <= number; index++){
+			factorial = factorial * index;
+		}
+		return factorial;
+	}
+
+	
 }
--- Main.java
+++ Main.java
@@ -7,9 +7,18 @@ public class Main{
 
 	Kata kata = new Kata();
 
-	System.out.println(kata.isEven());
-	System.out.println(kata.isPrimeNumber());
+	System.out.println("Enter a number to check if it is an even number: ");
+	int number = input.nextInt();
+	boolean isNumberEven = kata.isEven(number);
+	System.out.println(isNumberEven);	
+
+
+	System.out.println("Enter a number to check if it is a prime number: ");
+	int num = input.nextInt();
+	boolean isNumberAPrimeNumber = kata.isPrimeNumber(number);
+	System.out.println(isNumberAPrimeNumber);
 	
+
 	System.out.println("Enter first number to find difference: ");
 	int number1 = input.nextInt();
 	System.out.println("Enter second number: ");
@@ -24,7 +33,31 @@ public class Main{
 	int num2 = input.nextInt();
 
 	double quotientNumber = kata.quotient(num1, num2);
-	System.out.printf("%.2f", quotientNumber);
+	System.out.printf("%.2f%n", quotientNumber);
+
+	System.out.println("Enter a number to check if it is a perfect square: ");
+	int numberSqr = input.nextInt();
+	boolean checkNumberSqr = kata.isPerfectSquare(numberSqr);
+	System.out.println(checkNumberSqr);
+
+	System.out.print("Enter a number to print its factors: ");
+	int numberFactor = input.nextInt();
+
+	kata.factorsOfNumbers(numberFactor);
+	
+
+	System.out.println("Enter a five-digit number to check if it is a palindrome");
+	int fiveDigit = input.nextInt();
+
+	boolean isFiveDigitPalindrome = kata.isPalindrome(fiveDigit);
+	System.out.println(isFiveDigitPalindrome);
+
+	System.out.print("Enter a number to check its factorial: ");
+	int numberFactorial = input.nextInt();
+
+	long printFactorial = kata.factorial(numberFactorial);
+	System.out.print(printFactorial);
+	
    }
 
 	
