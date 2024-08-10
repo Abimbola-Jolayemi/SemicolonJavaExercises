940ac169dbd88b1b229726b2149bde8feb393c53
Abimbola Jolayemi <abimbolajolayemi111@yahoo.com>
2024-08-10 09:43:38 +0100

    removed addtocart file

--- Gate One/CheckOut System/AddToCart.java
+++
@@ -1,9 +0,0 @@
-public class AddToCart{
-   private ArrayList<CheckOutFunction> cart = new ArrayList<>();
-
-	public void addProductToCart(String productName, double price, int quantity, double total){
-		CheckOutFunction checkout = new CheckOutFunction(productName, price, quantity, total);
-
-		cart.add(product);
-	}
-}
